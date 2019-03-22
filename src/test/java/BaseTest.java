af4b28a71df2193f45ee033c866e9d966dcca0f2
SergeyBudaragin <lestate@rambler.ru>
2019-03-22 18:27:53 +0300

    1

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -20,19 +20,19 @@ public class BaseTest {
         switch (properties.getProperty("browser")) {
             case "Chrome": {
                 System.out.println("Выбран Chrome");
-                System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.chrome.driver"));
                 Init.setDriver(new ChromeDriver());
                 break;
             }
             case "Firefox": {
                 System.out.println("Выбран Firefox");
-                System.setProperty("webdriver.gecko.driver", "drv/geckodriver.exe");
+                System.setProperty("webdriver.gecko.driver", properties.getProperty("webdriver.gecko.driver"));
                 Init.setDriver(new FirefoxDriver());
                 break;
             }
             case "IE": {
                 System.out.println("Выбран IE");
-                System.setProperty("webdriver.ie.driver", "drv/IEDriverServer.exe");
+                System.setProperty("webdriver.ie.driver", properties.getProperty("webdriver.ie.driver"));
                 Init.setDriver(new InternetExplorerDriver());
                 break;
             }
